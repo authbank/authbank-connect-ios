cd11e16ec0458c19aaf44358e0141a56c7b89a2c
Amarildo Lucas <vmarildo@gmail.com>
2021-06-20 15:51:08 -0300

    updated binary target url and checksum

--- Package.swift
+++ Package.swift
@@ -5,14 +5,11 @@ import PackageDescription
 
 let package = Package(
     name: "AuthBankConnect",
-    platforms: [
-        .iOS(.v13)
-    ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(name: "AuthBankConnect",
-                      url: "https://github.com/authbank/authbank-connect-ios/releases/download/v0.0.1-alpha/AuthBankConnect.xcframework.zip",
-                      checksum: "8cd866375ee860875f17509375a0d75c0a18df840b6f0583fd32fb7269641112")
+                      url: "https://github.com/authbank/authbank-connect-ios/releases/download/v0.0.2-alpha/AuthBankConnect.xcframework.zip",
+                      checksum: "fbd4a72789843bfde436e4d1cad1989e1a2a528c50c570fa22756400771b1b54")
     ]
 )
