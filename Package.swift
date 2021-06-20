ae1e8025b242e79f25dd900718816aa703eda417
Amarildo Lucas <vmarildo@gmail.com>
2021-06-20 15:55:11 -0300

    added platforms version

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "AuthBankConnect",
+    platforms: [
+        .iOS(.v14)
+    ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
