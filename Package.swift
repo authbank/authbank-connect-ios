489ede528233d413f82351640615ddfc1a33e8a5
Amarildo Lucas <vmarildo@gmail.com>
2021-06-20 15:08:25 -0300

    updated binary target url

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(name: "AuthBankConnect",
-                      url: "https://github.com/authbank/authbank-connect-ios/releases/download/v0.0.1-alpha/AuthBankConnect.xcframework.zip",
+                      url: "https://github.com/authbank/authbank-connect-ios/releases/download/v0.0.4-alpha/AuthBankConnect.xcframework.zip",
                       checksum: "395129f3d898c584c1c826c1ed89b8c62093d003dfafdfc47ffce129d1c4247c")
     ]
 )
